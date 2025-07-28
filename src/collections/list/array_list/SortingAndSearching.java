92f2f6690d6fc1307c94d53926b9d1498867299c
Gangaraju K R <gangagangaraju1@gmail.com>
2025-07-28 21:58:05 +0530

    :feature: added reversing list sorting and searching

--- src/collections/list/array_list/SortingAndSearching.java
+++ src/collections/list/array_list/SortingAndSearching.java
@@ -1,5 +1,25 @@
 package collections.list.array_list;
 
-public class SortingAndSearching {
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
+public class SortingAndSearching {
+	public static void main(String[] args) {
+		List<String> al = new ArrayList<>();
+//		String
+
+		al.add("Gangaraju");
+		al.add("Pavana");
+		al.add("Thejas");
+		al.add("Renuka");
+		al.add("Yuva");
+		al.add("Pavana");
+		al.add("Araadhya");
+		
+//		Collections.sort(al);
+		
+		
+//		System.out.println(al.sort());
+	}
 }
---
+++ src/stream_api/HigherBasics.java
@@ -0,0 +1,39 @@
+package stream_api;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+public class HigherBasics {
+
+	public static void main(String[] args) {
+		int[] arr = {2, 3, 4, 5, 9, 5, 6, 8, 19, 33, 22, 786, 45, 5};
+//		System.out.println(filterEvenNumbers(arr));
+		System.out.println(numCount(arr));
+		
+	}
+	
+	//Filter even numbers from an integer array.
+	public static List<Integer> filterEvenNumbers(int[] arr) {
+		//return type List<Integer>
+		return Arrays.stream(arr)
+				.filter(a -> a % 2 == 0)
+				.boxed()
+				.collect(Collectors.toList());
+		
+//		// return type int[]
+//		return Arrays.stream(arr)
+//				.filter(a -> a % 2 == 0)
+//				.toArray();
+	}
+	
+	//Count how many times each number appears in a list.
+	public static Map<Integer, Long> numCount(int[] arr) {
+		return Arrays.stream(arr)
+				.boxed()
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+	}
+}
